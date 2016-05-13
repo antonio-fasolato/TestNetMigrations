a7e169ab9f12b890abcfe660c8cf108e7860391a
ut0729 <fasolato@omicronconsulting.it>
2016-05-13 16:43:56 +0200

    [NEW] Migration with add column

---
+++ MigrationsRepository/Migration2AddDateToUsers.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace MigrationsRepository
+{
+    [Migration(2)]
+    public class Migration2AddDateToUsers : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("users").AddColumn("birthdate").AsDate().Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("birthdate").FromTable("users");
+        }
+    }
+}

6d6ee183266ecb5624e9e5eaadf343f29a2b21cb
ut0729 <fasolato@omicronconsulting.it>
2016-05-13 16:38:12 +0200

    [NEW] First migration

---
+++ MigrationsRepository/Migration1Users.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+
+namespace MigrationsRepository
+{
+    [Migration(1)]
+    public class Migration1Users : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("users")
+                .WithColumn("id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("name").AsString().NotNullable()
+                .WithColumn("password").AsString().NotNullable();
+
+            Create.Index().OnTable("users").OnColumn("name").Ascending().WithOptions().Unique();
+
+            Insert.IntoTable("users").Row(new {name = "admin", password = "admin"});
+        }
+
+        public override void Down()
+        {
+            Delete.Table("users");
+        }
+    }
+}

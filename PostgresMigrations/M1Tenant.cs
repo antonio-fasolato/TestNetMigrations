c27975b657f8d823ddba7cb1f784209511dc303f
Fasolato Antonio <fasolato@omicronconsulting.it>
2016-05-16 16:32:18 +0200

    [NEW] Migrations run from test project

---
+++ PostgresMigrations/M1Tenant.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+
+namespace PostgresMigrations
+{
+    [Migration(1)]
+    public class M1Tenant : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("tenant")
+                .WithColumn("id").AsGuid().NotNullable().PrimaryKey()
+                .WithColumn("urldomain").AsString().NotNullable()
+                .WithColumn("enabled").AsBoolean()
+                .WithColumn("createdat").AsDateTime()
+                .WithColumn("changedat").AsDateTime()
+                .WithColumn("createdby").AsString()
+                .WithColumn("changedby").AsString()
+                .WithColumn("serializeddto").AsString().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("tenant");
+        }
+    }
+}
---
+++ TestPostgresMigrations/Migrate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Configuration;
+using FluentMigrator;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+using FluentMigrator.Runner.Processors.Postgres;
+using PostgresMigrations;
+using Xunit;
+
+namespace TestPostgresMigrations
+{
+    public class Migrate
+    {
+        public class MigrationsOptions : IMigrationProcessorOptions
+        {
+            public bool PreviewOnly { get; set; }
+            public int Timeout { get; set; }
+            public string ProviderSwitches { get; set; }
+        }
+
+        [Fact]
+        public void Down()
+        {
+            var announcer = new TextWriterAnnouncer(Console.WriteLine);
+            var assembly = typeof(M1Tenant).Assembly;
+
+            var migrationContext = new RunnerContext(announcer)
+            {
+                Namespace = "PostgresMigrations"
+            };
+
+            var options = new MigrationsOptions {PreviewOnly = false, Timeout = 60};
+            var factory = new PostgresProcessorFactory();
+            using (
+                var processor = factory.Create(ConfigurationManager.ConnectionStrings["Migrations"].ConnectionString,
+                    announcer, options))
+            {
+                var runner = new MigrationRunner(assembly, migrationContext, processor);
+                runner.MigrateDown(0, true);
+            }
+        }
+
+        [Fact]
+        public void Up()
+        {
+            var announcer = new TextWriterAnnouncer(Console.WriteLine);
+            var assembly = typeof(M1Tenant).Assembly;
+
+            var migrationContext = new RunnerContext(announcer)
+            {
+                Namespace = "PostgresMigrations"
+            };
+
+            var options = new MigrationsOptions {PreviewOnly = false, Timeout = 60};
+            var factory = new PostgresProcessorFactory();
+            using (
+                var processor = factory.Create(ConfigurationManager.ConnectionStrings["Migrations"].ConnectionString,
+                    announcer, options))
+            {
+                var runner = new MigrationRunner(assembly, migrationContext, processor);
+                runner.MigrateUp(true);
+            }
+        }
+    }
+}
